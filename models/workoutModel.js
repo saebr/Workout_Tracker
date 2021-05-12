be496e44b16368adde5f6dd8a0b4a73fa118c328
Saebr <bensaebr@gmail.com>
2021-05-12 15:38:21 -0500

    added functionallity for saving workouts

--- models/workoutModel.js
+++ models/workoutModel.js
@@ -6,13 +6,13 @@ const WorkoutSchema = new Schema ({
     day: {type: Date, default: Date.now},
     exercises: [
       {
-        type: String,
-        name: String,
-        duration: Number,
-        weight: Number,
-        reps: Number,
-        sets: Number,
-        distance: Number,
+        type: {type: String},
+        name: {type: String},
+        duration: {type: Number},
+        weight: {type: Number},
+        reps: {type: Number},
+        sets: {type: Number},
+        distance: {type: Number},
       },
     ],
 });
--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,34 +1,59 @@
 const router = require('express').Router();
 const Workout = require('../models/workoutModel.js');
 
-router.get('/api/workouts', async (req, res) => {
-    try {
-        const workoutData = await Workout.findAll();
-        return res.status(200).json(workoutData);
-      } catch (err) {
-        res.status(500).json(err);
-      }
+router.get('/api/workouts', (req, res) => {
+      Workout.aggregate([
+        { $addFields:{totalDuration:{
+          $sum:"$exercises.duration"
+        }}}
+      ]).then((data) => {
+        console.log(data)
+        return res.status(200).json(data)
+      })
+      .catch((err) => {
+          return res.status(500).json(err)
+      });
     });
 
-    router.get('/api/workouts/range', async (req, res) => {
-        try {
-            const workoutData = await Workout.findAll();
-            return res.status(200).json(workoutData);
-          } catch (err) {
-            res.status(500).json(err);
-          }
-        });
+    router.get('/api/workouts/range', (req, res) => {
+      Workout.aggregate([
+        { $addFields:{totalDuration:{
+          $sum:"$exercises.duration"
+        }}}
+      ]).then((data) => {
+        console.log(data)
+        return res.status(200).json(data)
+      })
+      .catch((err) => {
+          return res.status(500).json(err)
+      });
+    });
 
     router.put('/api/workouts/:id', (req, res) => {
+        console.log(req.body)
         // update a category by its `id` value
         Workout.updateOne({_id:req.params.id},{$push: {exercises: req.body}})
         .then((data) => {
+          console.log(data)
           return res.status(200).json(data)
         })
         .catch((err) => {
+          console.log(err)
             return res.status(500).json(err)
         })
         
       });
+
+    router.post('/api/workouts', (req, res) => {
+      console.log(req.body)
+      Workout.create({}).then((data) => {
+        console.log(data)
+        return res.status(200).json(data)
+      })
+      .catch((err) => {
+          return res.status(500).json(err)
+      })
+    
+    });
 
       module.exports = router;
